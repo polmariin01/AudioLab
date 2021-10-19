d182d39253d0cf26a05cd7fa835e0cec8ad1511e
gilboleda <gilboleda@gmail.com>
2021-10-19 17:59:32 +0200

    mod 2

--- wp2.m
+++ wp2.m
@@ -21,17 +21,15 @@ y = 10*x+soroll;
 %mesura de la THD 
 A = 1;
 z = A*sin(2*pi*n*(f/fs)); %senyal original
-k = z+ A/2*sin(2*pi*n*(2*f/fs))+ A/4*sin(2*pi*n*(3*f/fs))+ A/8*sin(2*pi*n*(4*f/fs)); %senyal amb distorsió interharmònica
+k = z+ A/4*sin(2*pi*n*(2*f/fs))+ A/8*sin(2*pi*n*(3*f/fs))+ A/32*sin(2*pi*n*(4*f/fs)); %senyal amb distorsió interharmònica
 Kf = fft(k);
-%plot(n,abs(Kf));
+plot(n,abs(Kf));
 [a,b] = max(abs(Kf(1:mostres/2)));
-
-cicles = floor((mostres/2)/(b-1));
-valors = 0:1:cicles-2;
+cicles = floor((mostres/2)/(b-1)); %utilitzem (b-1) perquè en el vector el primer índex es 1
+valors = 0:1:cicles-2; %creem un vector buit per posar-hi els valors de les amplituds als diferents harmònics
 for i = 2:cicles
-    %[valor,x] = max(abs(Kf(i*b-4:2*i+4)));
-    valors(i) = abs(Kf(i*(b-1)+1));
-    
+    l = i*(b-1)+1;
+    [valors(i),c] = max(abs(Kf(l-4:l+4)));
 end
 l = sum(valors*valors');
 THD = sqrt(l)/a
