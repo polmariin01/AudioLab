936b8165334acea3a7ec0afbbdcd56dbfb121ed3
Joan88888 <67022756+Joan88888@users.noreply.github.com>
2021-12-19 22:43:20 +0100

    Add files via upload

--- resposta_frec_sinusoides.m
+++ resposta_frec_sinusoides.m
@@ -1,20 +1,31 @@
 clear;
-D = 0.1;
-fs = 10000000;
+D = 1;
+fs = 192000;
 t = 0:(1/fs):D-(1/fs);    
 n = 1:1:D*fs;
 C = 470e-9;
 L = 22e-6;
 R = 8;
-N = 1000;
+N = 100;
 freq = logspace(0,5,N);
 guanys = zeros(1,N);
 senyal = 0;
 
 for i = 1:N
+    figure(1);
     senyal = sin(2*pi*freq(i)*t);
-    filtrada = funcions.lpf(senyal,R,L,C,fs);
-    guanys(i) = max(filtrada)/max(senyal);
+    hold on;
+    player = (audioplayer(senyal,fs,16));
+    recorder = audiorecorder(fs, 16, 1);
+    record(recorder);
+    playblocking(player);
+    stop(recorder);
+    filtrada = getaudiodata(recorder, 'single');
+    i
+    guany = funcions.guany(senyal,filtrada);
+    guanys(i) = guany;
+    figure(2);
+    semilogx(freq,log10(guanys));
 end
 
 semilogx(freq,log10(guanys));
